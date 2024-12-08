78c586d694bb1095c02e007ef925e004e0bf58a6
Muhammad Azhar <160399289+muhammadazhar17@users.noreply.github.com>
2024-12-08 23:12:25 +0500

    Update page.tsx

--- src/app/ourChefs/page.tsx
+++ src/app/ourChefs/page.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Hero from "./Hero"
 import ChefGrid from "./ChefGrid"
-import chef1 from "../assets/Chef Card 1.png";
-import chef2 from "../assets/Card2.png";
-import chef3 from "../assets/Card3.png";
-import chef4 from "../assets/Card4.png";
+import chef1 from "../assets/bakery1.png";
+import chef2 from "../assets/bakery2.png";
+import chef3 from "../assets/bakery3.png";
+import chef4 from "../assets/bakery4.png";
 import Image from "next/image"
 
 const OurChefs = () => {
