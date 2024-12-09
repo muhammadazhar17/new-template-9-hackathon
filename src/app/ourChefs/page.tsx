48bc48cbd1b9375e7fda1689a13d486e1b1f37cd
Muhammad Azhar <160399289+muhammadazhar17@users.noreply.github.com>
2024-12-09 18:08:36 +0500

    Update page.tsx

--- src/app/ourChefs/page.tsx
+++ src/app/ourChefs/page.tsx
@@ -24,10 +24,10 @@ const OurChefs = () => {
         </h1>
         </div>
         <div className="flex flex-col md:flex-row justify-center items-center gap-[20px] mt-[50px]">
-          <Image src={chef1} alt="" className="w-[200px] md:w-[305px] md:h-[280px] cursor-pointer"/>
-          <Image src={chef2} alt="" className="w-[200px] md:w-[280px] md:h-[280px] cursor-pointer" />
-          <Image src={chef3} alt="" className="w-[200px] md:w-[280px] md:h-[280px] cursor-pointer" />
-          <Image src={chef3} alt="" className="w-[200px] md:w-[280px] md:h-[280px] cursor-pointer" />
+          <Image src={bakery1} alt="" className="w-[200px] md:w-[305px] md:h-[280px] cursor-pointer"/>
+          <Image src={bakery2} alt="" className="w-[200px] md:w-[280px] md:h-[280px] cursor-pointer" />
+          <Image src={bakery3} alt="" className="w-[200px] md:w-[280px] md:h-[280px] cursor-pointer" />
+          <Image src={bakery4} alt="" className="w-[200px] md:w-[280px] md:h-[280px] cursor-pointer" />
         </div>
       </section>
     </div>
